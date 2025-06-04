088204d1d841fd7c42ddc2355b25de840b487bd7
Bhargava Shastry <bhargava.shastry@ethereum.org>
2025-06-04 11:19:12 +0200

    fix: Correct GH Pages config and add deployment workflow

--- next.config.js
+++ next.config.js
@@ -5,8 +5,6 @@ const nextConfig = {
   images: {
     unoptimized: true
   },
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/bshastry.github.io' : '',
-  basePath: process.env.NODE_ENV === 'production' ? '/bshastry.github.io' : '',
 }
 
 module.exports = nextConfig
